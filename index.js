5215e6f2bed5c00d186d19a56a29fa2296868c7f
rapforgeia-boop <rapforgeia@gmail.com>
2026-04-17 03:01:13 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🔥 RapForge API ONLINE 🔥");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
